d31083313bb22986d6d0f3d50f5779d42cffde57
jlcrawford9588 <jlcrawford9588@gmail.ccom>
2020-07-11 22:01:48 -0500

    JC - Adding getters and setters to UnitOfMeasure

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -13,4 +13,20 @@ public class UnitOfMeasure {
     Long id;
 
     String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
 }
